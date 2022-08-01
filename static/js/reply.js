4902e833c2bb2c96799a50dacd0a8bcc2479ad43
m1nsuppp <m1nsuppp@gmail.com>
2022-08-01 23:03:09 +0900

    댓글 달기 함수 replySubmit() 구현

--- static/js/reply.js
+++ static/js/reply.js
@@ -4,26 +4,31 @@ function dateToYYYYMMDDHHMMSS() {
   return new Date(+new Date() + 3240 * 10000).toISOString().replace("T", " ").replace(/\..*/, '');
 }
 
-function main() {
+async function replySubmit() {
   const queryString = new URLSearchParams(window.location.search);
   const formReply = document.getElementById('form-reply');
+  const URL = '/detail/reply';
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      queryString: queryString.get('id'),
+      replyContent: formReply.value,
+      date: dateToYYYYMMDDHHMMSS(),
+    }),
+  };
+
+  let response = await fetch(URL, options).catch(error => console.log(error));
+  let result = await response.json().catch(error => console.log(error));
+  console.log(result);
+}
+
+function main() {
   const replySubmitBtn = document.getElementById('reply-submit-btn');
 
-  replySubmitBtn.addEventListener('click', async (event) => {
-    let response = await fetch('/detail/reply', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        queryString: queryString.get('id'),
-        replyContent: formReply.value,
-        date: dateToYYYYMMDDHHMMSS(),
-      }),
-    }).catch(error => console.log(error));
-    let result = await response.json().catch(error => console.log(error));
-    console.log(result);
-  })
+  replySubmitBtn.addEventListener('click', replySubmit);
 }
 
 main();

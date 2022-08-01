b1c508fcc20b02827737ba96ec74748003f8cdb1
m1nsuppp <m1nsuppp@gmail.com>
2022-08-02 00:27:29 +0900

    reply DB key값 변경

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ def home():
 @app.route('/detail', methods=["GET"])
 def detail():
     query_string = request.args.get('id')
-    board = db.board.find_one({'_id': ObjectId(query_string)})
-    reply = db.reply.find({'board_id': query_string})
+    board = db.board.find_one({ '_id': ObjectId(query_string) })
+    reply = db.reply.find({ 'boardId': query_string} )
     return render_template('detail.html', board = board, reply = reply, query_string = query_string)
 
 @app.route('/detail/reply', methods=["POST"])
@@ -25,19 +25,17 @@ def reply():
     params = request.get_json()
     print(params['replyContent'])
     print(params['date'])
-    print(params['queryString'])
+    print(params['boardId'])
 
     doc = {
-      'board_id': params['queryString'],
+      'boardId': params['boardId'],
       'replyContent': params['replyContent'],
       'date': params['date'],
     }
 
     db.reply.insert_one(doc)
 
-    return jsonify({
-      'msg': 'reply upload success',
-    })
+    return jsonify({ 'msg': 'reply upload success', })
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port = 5000, debug = True)

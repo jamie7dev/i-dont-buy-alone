1fdc66e7bb72f3efb1978079eb9feb5a8a048ebc
DongDongShin <100353794+DongDongShin@users.noreply.github.com>
2022-08-02 14:49:27 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,81 +1,108 @@
-from flask import Flask, render_template, jsonify, request
-from bson.objectid import ObjectId
 from pymongo import MongoClient
-import requests
+import jwt
+import datetime
+import hashlib
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
 
-import certifi
-
-ca = certifi.where()
-
-client = MongoClient('mongodb+srv://lee1231234:lee1231234@cluster0.vms1u.mongodb.net/?retryWrites=true&w=majority',
-                     tlsCAFile=ca)
-db = client.dbibla
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
+SECRET_KEY = 'SPARTA'
 
-##일단 사용하지만 처음은 login화면이여야함
-@app.route('/', methods=["GET"])
+client = MongoClient('mongodb+srv://test:test@cluster0.0l29x.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
+@app.route('/')
 def home():
-    dbposts = list(db.board.find({}))
-    category = list(db.category.find({}, {'_id': False}))
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        return render_template('index.html')
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
 
-    return render_template("post.html", dbposts=dbposts, category=category)
-
-
-@app.route('/detail', methods=["GET"])
-def detail():
-    query_string = request.args.get('id')
-    board = db.board.find_one({'_id': ObjectId(query_string)})
-    reply = db.reply.find({'boardId': query_string})
-    return render_template('detail.html', board=board, reply=reply, query_string=query_string)
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
 
-@app.route('/detail/reply', methods=["POST"])
-def reply():
-    params = request.get_json()
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+@app.route('/signin')
+def signin():
+    msg = request.args.get("msg")
+    return render_template('signin.html', msg=msg)
 
 
+@app.route('/user/<username>')
+def user(username):
+    # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
+
+        user_info = db.users.find_one({"username": username}, {"_id": False})
+        return render_template('user.html', user_info=user_info, status=status)
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+    @app.route('/sign_up/check_dup', methods=['POST'])
+    def check_dup():
+        username_receive = request.form['username_give']
+        exists = bool(db.users.find_one({"username": username_receive}))
+        return jsonify({'result': 'success', 'exists': exists})
+
+
+
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    accountEmail = request.form['userEmail_give']
+    pw = request.form['password_give']
+    phone = request.form['password_give']
+    nickname = request.form['nickname_give']
+    password_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
     doc = {
-        'boardId': params['boardId'],
-        'replyContent': params['replyContent'],
-        'date': params['date'],
+        "accountEmail": accountEmail,
+        "pw": password_hash,
+        "nickname": nickname,
+        "phone":  phone
     }
-
-    db.reply.insert_one(doc)
-
-    return jsonify({'msg': 'reply upload success', })
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
 
 
-######Lee1231234 make here######
-##카테고리 인덱스만 찾기
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"accountEmail": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
 
-@app.route('/index/<keyword>')
-def find_index(keyword):
-    if keyword.isdigit():
-        dbposts = list(db.board.find({}))
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'accountEmail': username_receive, 'pw': pw_hash})
+
+    if result is not None:
+        payload = {
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
     else:
-        dbposts = list(db.board.find({"category": keyword}))
-    category = list(db.category.find({}, {'_id': False}))
-    return render_template("post.html", dbposts=dbposts, category=category)
-
-
-##전체 인덱스 찾기
-@app.route('/index/', methods=['GET'])
-def view_index():
-    # 인덱스 형성
-    dbposts = list(db.board.find({}))
-    category = list(db.category.find({}, {'_id': False}))
-
-    return render_template("post.html", dbposts=dbposts, category=category)
-
-@app.route('/search/', methods=['GET'])
-def search_index():
-    title_receive = request.args.get('title_give')
-    dbposts=list(db.board.find({"title": {'$regex' : '.*' +title_receive+ '.*'}}))
-    category = list(db.category.find({}, {'_id': False}))
-    return render_template("post.html",dbposts=dbposts, category=category)
-
-######Lee1231234 make end######
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)

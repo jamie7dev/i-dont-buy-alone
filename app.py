57803c3b5b8f1b58d9e11930cfaa6e4064b5a975
Lee1231234 <qscgyd1629@naver.com>
2022-08-02 11:05:58 +0900

    commit new Lee1231234

--- app.py
+++ app.py
@@ -1,24 +1,34 @@
 from flask import Flask, render_template, jsonify, request
 from bson.objectid import ObjectId
 from pymongo import MongoClient
-from mongo_info import connect
+import requests
 
-client = MongoClient(connect)
+import certifi
+
+ca = certifi.where()
+
+client = MongoClient('mongodb+srv://lee1231234:lee1231234@cluster0.vms1u.mongodb.net/?retryWrites=true&w=majority',
+                     tlsCAFile=ca)
 db = client.dbibla
-
 app = Flask(__name__)
 
+
+##일단 사용하지만 처음은 login화면이여야함
 @app.route('/', methods=["GET"])
 def home():
-    board = list(db.board.find({},))
-    return render_template('index.html', board = board)
+    dbposts = list(db.board.find({}))
+    category = list(db.category.find({}, {'_id': False}))
+    print(dbposts)
+    return render_template("post.html", dbposts=dbposts, category=category)
+
 
 @app.route('/detail', methods=["GET"])
 def detail():
     query_string = request.args.get('id')
-    board = db.board.find_one({ '_id': ObjectId(query_string) })
-    reply = db.reply.find({ 'boardId': query_string} )
-    return render_template('detail.html', board = board, reply = reply, query_string = query_string)
+    board = db.board.find_one({'_id': ObjectId(query_string)})
+    reply = db.reply.find({'boardId': query_string})
+    return render_template('detail.html', board=board, reply=reply, query_string=query_string)
+
 
 @app.route('/detail/reply', methods=["POST"])
 def reply():
@@ -28,16 +38,39 @@ def reply():
     print(params['boardId'])
 
     doc = {
-      'boardId': params['boardId'],
-      'replyContent': params['replyContent'],
-      'date': params['date'],
+        'boardId': params['boardId'],
+        'replyContent': params['replyContent'],
+        'date': params['date'],
     }
 
     db.reply.insert_one(doc)
 
-    return jsonify({ 'msg': 'reply upload success', })
+    return jsonify({'msg': 'reply upload success', })
+
+
+######Lee1231234 make here######
+##카테고리 인덱스만 찾기
+@app.route('/index/<keyword>')
+def find_index(keyword):
+    if keyword.isdigit():
+        dbposts = list(db.board.find({}))
+    else:
+        dbposts = list(db.board.find({"category": keyword}))
+    category = list(db.category.find({}, {'_id': False}))
+    return render_template("post.html", dbposts=dbposts, category=category)
+
+
+##전체 인덱스 찾기
+@app.route('/index/', methods=['GET'])
+def view_index():
+    # 인덱스 형성
+    dbposts = list(db.board.find({}))
+    category = list(db.category.find({}, {'_id': False}))
+    print(dbposts)
+    return render_template("post.html", dbposts=dbposts, category=category)
+
+
+######Lee1231234 make end######
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port = 5000, debug = True)
-
-
+    app.run('0.0.0.0', port=5000, debug=True)

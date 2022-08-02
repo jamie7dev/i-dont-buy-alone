5af1c8afe3c2e690071a90809216ab1142646f8c
Lee1231234 <qscgyd1629@naver.com>
2022-08-02 21:09:18 +0900

    인증 추가 upload 테이블 제외

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+import certifi
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from bson.objectid import ObjectId
 from pymongo import MongoClient
-from mongo_info import connect
+##from mongo_info import connect
 import jwt, hashlib, json
 from datetime import datetime
 from datetime import timedelta
 
-client = MongoClient(connect)
+ca = certifi.where()
+
+client = MongoClient('mongodb+srv://lee1231234:lee1231234@cluster0.vms1u.mongodb.net/?retryWrites=true&w=majority',
+                     tlsCAFile=ca)
 db = client.dbibla
 
 app = Flask(__name__)
@@ -14,9 +18,24 @@ app = Flask(__name__)
 SECRET_KEY = 'team7'
 
 ######Lee1231234 make here######
+
+def auth_cookie():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+
+        user_info = db.account.find_one({"accountEmail": payload["accountEmail"]})
+    except jwt.ExpiredSignatureError:
+        return True
+    except jwt.exceptions.DecodeError:
+        return True
+
 ##전체 인덱스 찾기
 @app.route('/', methods=['GET'])
 def view_index():
+
+    if auth_cookie():
+        return redirect(url_for("render_signin", msg="로그인이 필요합니다."))
     # 인덱스 형성
     dbposts = list(db.board.find({}))
     category = list(db.category.find({}, {'_id': False}))
@@ -25,6 +44,10 @@ def view_index():
 ##카테고리 인덱스만 찾기
 @app.route('/<keyword>')
 def find_index(keyword):
+
+    if auth_cookie():
+        return redirect(url_for("render_signin", msg="로그인이 필요합니다."))
+
     if keyword.isdigit():
         dbposts = list(db.board.find({}))
     else:
@@ -34,6 +57,10 @@ def find_index(keyword):
 
 @app.route('/search/', methods=['GET'])
 def search_index():
+
+    if auth_cookie():
+        return redirect(url_for("render_signin", msg="로그인이 필요합니다."))
+
     title_receive = request.args.get('title_give')
     dbposts=list(db.board.find({"title": {'$regex' : '.*' +title_receive+ '.*'}}))
     category = list(db.category.find({}, {'_id': False}))
@@ -43,6 +70,10 @@ def search_index():
 ################################## DETAIL ##################################
 @app.route('/detail', methods=["GET"])
 def detail():
+
+    if auth_cookie():
+        return redirect(url_for("render_signin", msg="로그인이 필요합니다."))
+
     query_string = request.args.get('id')
     board = db.board.find_one({ '_id': ObjectId(query_string) })
     reply = db.reply.find({ 'boardId': query_string} )
@@ -51,6 +82,10 @@ def detail():
 
 @app.route('/detail/reply', methods=["POST"])
 def reply():
+
+    if auth_cookie():
+        return redirect(url_for("render_signin", msg="로그인이 필요합니다."))
+
     params = request.get_json()
 
     doc = {
@@ -127,39 +162,51 @@ def is_in_use_email():
 ########################### 업로드 ###########################     
 @app.route('/upload', methods=['GET'])
 def upload():
+
+    if auth_cookie():
+        return redirect(url_for("render_signin", msg="로그인이 필요합니다."))
     return render_template('upload.html')
 
 @app.route('/upload', methods=['POST'])
 def save_upload():
+
+    if auth_cookie():
+        return redirect(url_for("render_signin", msg="로그인이 필요합니다."))
+
     title_receive = request.form['title_give']
     content_receive = request.form['content_give']
     category_receive = request.form['category_give']
     price_receive = request.form['price_give']
     num_receive = request.form['num_give']
-
     file = request.files["file_give"]
 
     extension = file.filename.split('.')[-1]
     today = datetime.now()
     mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
-
+    mytime2= today.strftime('%m월 %d일 %H시 %M분')
     filename = f'file-{mytime}'
 
     save_to = f'static/{filename}.{extension}'
     file.save(save_to)
+    token_receive = request.cookies.get('mytoken')
+    payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
 
+    user_info = db.account.find_one({"accountEmail": payload["accountEmail"]})
     doc = {
+        'boardEmail' : user_info,
         'title': title_receive,
         'content': content_receive,
         'category': category_receive,
         'price': price_receive,
-        'num': num_receive,
-        'file': f'{filename}.{extension}'
+        'participant': num_receive,
+        'file': f'{filename}.{extension}',
+        'date' : mytime2
+
     }
 
-    db.upload.insert_one(doc)
+    db.board.insert_one(doc)
 
-    print(num_receive)
+
 
     return jsonify({'msg': ' 작성 완료!'})
 

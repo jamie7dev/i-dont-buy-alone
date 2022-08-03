ea52f6276b97eb0c57efb703b66e45cdb102de76
m1nsuppp <m1nsuppp@gmail.com>
2022-08-03 12:07:21 +0900

    프로필 조회 기능 함수 구현 예정

--- static/js/index.js
+++ static/js/index.js
@@ -2,9 +2,16 @@
 
 import { getCookie } from "./cookie.js";
 
+async function getProfile() {
+  console.log('zz');
+}
+
 function main() {
   const search = document.getElementById('search');
   const nickname = `${localStorage.getItem('nickname')}님 함께 나눠요💚`;
+  const profileBox = document.querySelector('.profile-box');
+
+  profileBox.addEventListener('click', getProfile);
   getCookie('mytoken') ? search.placeholder = nickname : search.placeholder;
 }
 

1152cddbd216f5487681ce065b72889ba1117beb
m1nsuppp <m1nsuppp@gmail.com>
2022-08-03 20:35:50 +0900

    프로필 이미지 업로드, 보여주기 기능 구현

--- static/js/profile.js
+++ static/js/profile.js
@@ -2,8 +2,29 @@
 
 import { onSignOut } from "./delete.js";
 
+async function uploadProfile(event) {
+  const formData = new FormData();
+  const URL = 'profile/img';
+
+  console.log(event.target.files[0]);
+
+  formData.append('profileImg', event.target.files[0]);
+  const options = {
+    method: 'POST',
+    body: formData,
+  };
+
+  let response = await fetch(URL, options).catch(error => console.log(error));
+  let result = await response.json().catch(error => console.log(error));
+
+  console.log(result);
+}
+
 function main() {
   const signOutBtn = document.getElementById('signout');
+  const inputProfile = document.getElementById('input-profile');
+
+  inputProfile.addEventListener('change', uploadProfile);
   signOutBtn.addEventListener('click', onSignOut);
 }
 

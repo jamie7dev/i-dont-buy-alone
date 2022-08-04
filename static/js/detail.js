c9f461c6d1914c196f0e7da9c94af31788c7ee10
Lee1231234 <qscgyd1629@naver.com>
2022-08-04 12:35:43 +0900

    다른 유저프로필 보이게 수정

--- static/js/detail.js
+++ static/js/detail.js
@@ -3,15 +3,11 @@
 import { showDeleteButton } from "./delete.js";
 import { replySubmit } from "./reply.js";
 
-function getProfile() {
-  window.location.href = '/profile';
-}
+
 
 function main() {
   const replySubmitBtn = document.getElementById('reply-submit-btn');
   document.addEventListener('DOMContentLoaded', showDeleteButton);
-  const profile = document.getElementById('uploader');
-  profile.addEventListener('click', getProfile)
   replySubmitBtn.addEventListener('click', replySubmit);
 }
 
